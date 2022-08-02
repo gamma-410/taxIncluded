e06e616d5266d115acca5c5254333be2e14fc7f5
gamma-410 <mail@gamma410.win>
2022-08-03 01:37:34 +0900

    git init && go build

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gamma410/taxIncluded
+
+go 1.18
---
+++ ti.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+  "fmt"
+  "flag"
+  "strconv"
+)
+
+func main() {
+  flag.Parse()
+  arg := flag.Arg(0)
+  i, _ := strconv.Atoi(arg)
+
+  fmt.Println("¥",taxIncluded(i))
+}
+
+func taxIncluded(i int) float64 {
+  f := float64(i)
+  consumptionTax := f * 0.1
+  taxIncludedPrice := f + consumptionTax
+
+  return taxIncludedPrice
+}
